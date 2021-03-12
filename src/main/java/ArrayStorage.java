4d9bc94e0d2bcea9fa9ae418ef98cdda295a16b0
vasyl_okhrimenko <v.okhrimenko@gmail.com>
2021-03-12 02:33:26 +0200

    [HW-04] Reworked the implementation of methods for storing employees in an array

--- src/main/java/ArrayStorage.java
+++ src/main/java/ArrayStorage.java
@@ -1,40 +1,57 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Employees
  */
 public class ArrayStorage {
+    private static int COUNT = 0;
     Employee[] storage = new Employee[10000];
 
     void clear() {
-        for (int i = size() - 1; i >= 0; i--) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, null);
+        COUNT = 0;
     }
 
     void save(Employee employee) {
-        storage[size()] = employee;
+
+        if (size() == storage.length) {
+            System.out.println("Unable to save. The array is full");
+        } else if (employee == null) {
+            System.out.println("Unable to save NULL");
+        } else {
+            COUNT++;
+            storage[size() - 1] = employee;
+        }
     }
 
     Employee get(String uuid) {
-        Employee e = null;
-        for (int i = 0; i <= size() - 1; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                e = storage[i];
+        int index = 0;
+        try {
+            while (!storage[index].uuid.equals(uuid)) {
+                if (index == storage.length - 1 || storage[index] == null) {
+                    return null;
+                }
+                index++;
             }
+        } catch (NullPointerException e) {
+            return null;
         }
-        return e;
+        return storage[index];
     }
 
     void delete(String uuid) {
-        int uuidIndex = 0;
-        for (int i = 0; i <= size() - 1; i++) {
+        Employee[] newArray = new Employee[storage.length];
+        for (int i = 0; i < size(); i++) {
             Employee e = storage[i];
             if (e.uuid.equals(uuid)) {
                 storage[i] = null;
-                uuidIndex = i;
+
+                System.arraycopy(storage, i + 1, newArray, 0, storage.length - 1 - i);
+                System.arraycopy(newArray, 0, storage, i, storage.length - 1 - i);
+
+                COUNT--;
+                break;
             }
-        }
-        for (int i = uuidIndex; i <= size() - 1; i++) {
-            storage[i] = storage[i + 1];
         }
     }
 
@@ -43,19 +60,11 @@ public class ArrayStorage {
      */
     Employee[] getAll() {
         Employee[] arrEmployee = new Employee[size()];
-        for (int i = 0; i <= size() - 1; i++) {
-            arrEmployee[i] = storage[i];
-        }
+        System.arraycopy(storage, 0, arrEmployee, 0, size());
         return arrEmployee;
     }
 
     int size() {
-        int realSize = 0;
-        for (Employee e : storage) {
-            if (e != null) {
-                realSize++;
-            }
-        }
-        return realSize;
+        return COUNT;
     }
 }
